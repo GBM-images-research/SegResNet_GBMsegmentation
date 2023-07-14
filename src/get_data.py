b3a6f1731fc42feec5abc704ac7e95e890e2a15a
amulet1989 <muletdelosreyes89@gmail.com>
2023-07-14 16:34:46 -0300

    update main

--- src/get_data.py
+++ src/get_data.py
@@ -3,12 +3,16 @@ from glob import glob
 from typing import List, Dict
 
 
-def get_file_data_label(data_path: str, label_path: str) -> List[Dict]:
+def get_file_data_label(
+    data_path: str, label_path: str, modalities: List[str] = []
+) -> List[Dict]:
     """Get the list of patient cases path as
     List[Dict]->[{'image':[paths of images], 'label':[paths of labels]}
     Arg:
          data_path(str): path directory, it has a folder by patient with MRIs
-         data_path(str): path directory to labels a .nni.gz by patient
+         label_path(str): path directory to labels a .nni.gz by patient
+         modalities(List[str]): List of modalities to include example ['T1.', 'T1GD.', ...].
+         if you want select all just setup modalities = [] or omit it
      return:
          dict_files(List[Dict]):  [{'image':[paths of images], 'label':[paths of labels]}]
     """
@@ -16,9 +20,17 @@ def get_file_data_label(data_path: str, label_path: str) -> List[Dict]:
     list_lab_ = sorted(os.listdir(label_path))
 
     list_data_files = []
-    for folder in list_data_:
-        data_mri = sorted(glob(os.path.join(data_path, folder, "*.nii.gz")))
-        list_data_files.append(data_mri)
+    if modalities == []:
+        for folder in list_data_:
+            data_mri = sorted(glob(os.path.join(data_path, folder, "*.nii.gz")))
+            list_data_files.append(data_mri)
+    else:
+        files = [f"*{mri}*nii.gz" for mri in modalities]
+        for folder in list_data_:
+            data_mri = []
+            for file in files:
+                data_mri += glob(os.path.join(data_path, folder, file))
+            list_data_files.append(data_mri)
 
     list_labels_files = []
     for file in list_lab_:
