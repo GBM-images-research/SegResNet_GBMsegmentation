c1503355cac3410c8d99e29bf63bd701f5f3481b
amulet1989 <muletdelosreyes89@gmail.com>
2023-09-17 16:15:51 -0300

    modificated ignore

--- src/config.py
+++ src/config.py
@@ -9,3 +9,7 @@ LABEL_TRAIN = str(Path(__file__).parent.parent / "Data/Label_train")
 
 DATA_TEST = str(Path(__file__).parent.parent / "Data/Data_test")
 LABEL_TEST = str(Path(__file__).parent.parent / "Data/Label_test")
+
+RAW_DATA_SET = str(Path(__file__).parent.parent / "Data/Raw_data_set")
+
+CASE_TEST = str(Path(__file__).parent.parent / "Data/Case_test")
