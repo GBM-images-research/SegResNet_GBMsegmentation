8d65e9e24c8056d3ff0f777c3e0e0566b1f01b58
amulet1989 <muletdelosreyes89@gmail.com>
2023-09-17 19:10:01 -0300

    added train_model.py

--- src/config.py
+++ src/config.py
@@ -2,14 +2,22 @@ import os
 from pathlib import Path
 
 
-MODEL_PATH = str(Path(__file__).parent.parent / "Data/Model")
+DATASET = str(Path(__file__).parent.parent / "Dataset")
+if not os.path.exists(DATASET):
+    os.mkdir(DATASET)
 
-DATA_TRAIN = str(Path(__file__).parent.parent / "Data/Data_train")
-LABEL_TRAIN = str(Path(__file__).parent.parent / "Data/Label_train")
+WEIGHTS = str(Path(__file__).parent.parent / "Weights")
+if not os.path.exists(WEIGHTS):
+    os.mkdir(WEIGHTS)
 
-DATA_TEST = str(Path(__file__).parent.parent / "Data/Data_test")
-LABEL_TEST = str(Path(__file__).parent.parent / "Data/Label_test")
+# MODEL_PATH = str(Path(__file__).parent.parent / "Data/Model")
 
-RAW_DATA_SET = str(Path(__file__).parent.parent / "Data/Raw_data_set")
+# DATA_TRAIN = str(Path(__file__).parent.parent / "Data/Data_train")
+# LABEL_TRAIN = str(Path(__file__).parent.parent / "Data/Label_train")
 
-CASE_TEST = str(Path(__file__).parent.parent / "Data/Case_test")
+# DATA_TEST = str(Path(__file__).parent.parent / "Data/Data_test")
+# LABEL_TEST = str(Path(__file__).parent.parent / "Data/Label_test")
+
+# RAW_DATA_SET = str(Path(__file__).parent.parent / "Data/Raw_data_set")
+
+# CASE_TEST = str(Path(__file__).parent.parent / "Data/Case_test")
