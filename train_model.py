037bb0a2d98a6dd9994cce3bc2c4152730b0f755
amulet1989 <muletdelosreyes89@gmail.com>
2023-09-18 17:00:31 -0300

    handle GPU memory

--- train_model.py
+++ train_model.py
@@ -343,7 +343,7 @@ def main():
                         val_data["image"].to(device),
                         val_data["label"].to(device),
                     )
-                    val_outputs = inference(val_inputs)
+                    val_outputs = inference(val_inputs, model)
                     val_outputs = [post_trans(i) for i in decollate_batch(val_outputs)]
                     dice_metric(y_pred=val_outputs, y=val_labels)
                     dice_metric_batch(y_pred=val_outputs, y=val_labels)
